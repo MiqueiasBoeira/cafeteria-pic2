7a058de966d6f13cf0a4195249b755c5664fcc9c
MiqueiasBoeira <mbomiqueias@gmail.com>
2023-11-18 11:19:36 -0300

    muda a URL de config.js para process.env.PORT || 8000

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 const port = process.env.PORT || 8000;
-export const BASE_URL_SERVER = `http://localhost:${port}`;
+export const BASE_URL_SERVER = port;
 export const BASE_URL_DATABASE = 'mongodb+srv://mickajp:<159Zx357>@cluster0.tlq3gtq.mongodb.net/?retryWrites=true&w=majority';
