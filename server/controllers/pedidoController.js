d15fbd2b985f463c6153944a71056aba33b82e06
MiqueiasBoeira <mbomiqueias@gmail.com>
2023-11-26 19:22:35 -0300

    atualizacao

--- server/controllers/pedidoController.js
+++ server/controllers/pedidoController.js
@@ -24,6 +24,8 @@ export async function criarPedido(req, res) {
         //const pedidoSalvo = await pedidosCollection.insertOne(novoPedido);
 
         //res.status(201).json(pedidoSalvo);
+
+        console.log(pedidoSalvo);
     } catch (err) {
         res.status(400).send.err;
     }
